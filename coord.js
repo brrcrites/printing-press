7c531fe809033262846ee21f36dfd61b9758ede5
Michael Bradley <mbrad94@gmail.com>
2019-06-27 14:05:25 -0700

    Update Coord inline docs to follow JS standard
    This standard is being used: https://make.wordpress.org/core/handbook/best-practices/inline-documentation-standards/javascript/

--- coord.js
+++ coord.js
@@ -1,15 +1,20 @@
 /**
  * A class meant to condense and ease the parsing of x, y pairs.
- * There is no specific meaning attached to x and y. I.e. they have no units, and can be used for distance,
- * position, etc.
+ *
+ * There is no specific meaning attached to x and y. I.e. they have no units,
+ * and can be used for distance, position, etc.
  */
 class Coord {
     x = -1;
     y = -1;
 
     /**
-     * @param x An integer value
-     * @param y An integer value
+     * @class
+     *
+     * @since 1.0.0
+     *
+     * @param {int} x   A value greater than 0.
+     * @param {int} y   A value greater than 0.
      */
     constructor(x, y) {
         this.setXY(x, y);
@@ -17,8 +22,11 @@ class Coord {
 
     /**
      * Set both x and y values.
-     * @param x An integer value
-     * @param y An integer value
+     *
+     * @since 1.0.0
+     *
+     * @param {int} x   A value greater than 0.
+     * @param {int} y   A value greater than 0.
      */
     setXY(x, y) {
         this.x = x;
@@ -26,7 +34,12 @@ class Coord {
     }
 
     /**
-     * Validate the x and y values. Both x and y must be positive.
+     * Validate the x and y values.
+     *
+     * Both x and y must be positive.
+     *
+     * @since 1.0.0
+     *
      * @returns {boolean} true if x and y are positive, false otherwise.
      */
     validate() {
