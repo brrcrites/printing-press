de7caad2d2338f54421917c91f77c6dc96b7aa7c
Michael Bradley <mbrad94@gmail.com>
2019-07-01 18:34:07 -0700

    Create Component class.
    Validate Component fields including Port objects according to ParchMint
    standard.
    
    Close #3

---
+++ component.js
@@ -0,0 +1,276 @@
+const ParchKey = require('./parch-key.js');
+const TestResult = require('./test-result.js');
+
+class Component extends ParchKey {
+
+    /**
+     * An array containing all of the Layers that this component exists on.
+     *
+     * @since   1.0.0
+     * @access  public
+     *
+     * @type    {Layer[]}
+     */
+    layers;
+
+    /**
+     * The amount of space this component takes up in the x direction.
+     *
+     * @since   1.0.0
+     * @access  public
+     *
+     * @type    {number}
+     */
+    xSpan;
+
+    /**
+     * The amount of space this component takes up in the y direction.
+     *
+     * @since   1.0.0
+     * @access  public
+     *
+     * @type    {number}
+     */
+    ySpan;
+
+    /**
+     * The type of component this component object represents.
+     *
+     * @since   1.0.0
+     * @access  public
+     *
+     * @type    {string}
+     */
+    entity;
+
+    /**
+     * An array containing all of the Ports on this component.
+     *
+     * @since   1.0.0
+     * @access  public
+     *
+     * @type    {Port[]}
+     */
+    ports;
+
+    /**
+     *
+     * Constructs the Component object.
+     *
+     * All parameters are optional. Strings default to ParchKey's default
+     * string. Spans default to DEFAULT_SPAN_VALUE. Arrays default to null.
+     *
+     * @class
+     * @augments ParchKey
+     *
+     * @since   1.0.0
+     *
+     * @see ParchKey.DEFAULT_STR_VALUE
+     *
+     * @param {string}  name    The name of the component.
+     * @param {string}  id      The unique id of the component.
+     * @param {Layer[]} layers  The Layers on which this component exists.
+     * @param {number}  xSpan   How much space this component takes up in the x direction.
+     * @param {number}  ySpan   How much space this component takes up in the y direction.
+     * @param {string}  entity  The type of component this component object represents.
+     * @param {Port[]}  ports   The ports on this component.
+     */
+    constructor(name = ParchKey.DEFAULT_STR_VALUE, id = ParchKey.DEFAULT_STR_VALUE, layers = null,
+                xSpan = Component.DEFAULT_SPAN_VALUE, ySpan = Component.DEFAULT_SPAN_VALUE,
+                entity = ParchKey.DEFAULT_STR_VALUE, ports = null) {
+
+        super(name, id);
+        this.layers = layers;
+        this.xSpan = xSpan;
+        this.ySpan = ySpan;
+        this.entity = entity;
+        this.ports = ports;
+
+    }
+
+    validate() {
+        let valid = super.validate();
+
+        valid = this.validateLayers() ? valid : false;
+        valid = this.validateSpan(this.xSpan, 'x') ? valid : false;
+        valid = this.validateSpan(this.ySpan, 'y') ? valid : false;
+        valid = this.validateEntity() ? valid : false;
+        valid = this.validatePorts() ? valid : false;
+
+        return valid;
+    }
+
+    /**
+     * Validates the layers field.
+     *
+     * No layer string can be empty. The layers field itself cannot be null.
+     *
+     * @since 1.0.0
+     *
+     * @returns {boolean}
+     */
+    validateLayers() {
+        let valid = true;
+
+        if (!this.layers) {
+            console.log('Component: Field "layers" is set to the default value.');
+            return false;
+        }
+
+        this.layers.forEach(function (value, index, array) {
+            if (value === '') {
+                valid = false;
+                console.log('Component: Field "layers" cannot have any empty strings. See layers[${index}].');
+            }
+        });
+
+        return valid;
+    }
+
+
+    /**
+     * Validates the given span value.
+     *
+     * Spans must not equal the default value. Spans must not be less than 1.
+     *
+     * @since 1.0.0
+     *
+     * @param {number}  value   The span value to validate.
+     * @param {string}  axis    The axis on which this span lies.
+     * @returns {boolean}
+     */
+    validateSpan(value, axis) {
+        switch (Component.testSpan(value)) {
+            case TestResult.DEFAULT:
+                console.log('Component: Field "' + axis + 'Span" is set to the default value.');
+                return false;
+
+            case TestResult.INVALID:
+                console.log('Component: Field "' + axis + 'Span" cannot be less than 1.');
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates the entity field.
+     *
+     * Follows the rules of ParchKey's test string function.
+     *
+     * @since 1.0.0
+     *
+     * @see ParchKey.testStringValue
+     *
+     * @returns {boolean}
+     */
+    validateEntity() {
+        return ParchKey.testStringValue(this.entity, 'entity', 'Component') === TestResult.VALID;
+    }
+
+    /**
+     * Validate the ports field.
+     *
+     * Calls the Port validation function on each port in the array. The ports field cannot be null.
+     *
+     * @since 1.0.0
+     *
+     * @returns {boolean}
+     */
+    validatePorts() {
+        let valid = true;
+
+        if (!this.ports) {
+            console.log('Component: Field "ports" is set to the default value.');
+            return false;
+        }
+
+        this.ports.forEach(function(value, index, array) {
+            valid = value.validate() ? valid : false;
+
+            for (let i = index + 1; i < array.length; i++) {
+                // Check duplicate port labels
+                if (value.label === array[i].label) {
+                    valid = false;
+                    console.log('Component: Field "ports" has duplicate labels (' + value.label + ') at indices ' +
+                            index + ' and ' + i + '.');
+                }
+
+                // Check overlapping port locations
+                if (value.pos.is(array[i].pos)) {
+                    valid = false;
+                    console.log('Component: Field "ports" has overlapping ports at location ' + value.pos + ' at' +
+                            ' indices ' + index + ' and ' + i + '.');
+                }
+            }
+
+
+        });
+
+        // Check invalid port locations
+        for (let i = 0; i < this.ports.length; i++) {
+            // The coord validation checks for negative #s so we only need to check for greater than spans here.
+            if (this.ports[i].pos.x > this.xSpan || this.ports[i].pos.y > this.ySpan) {
+                valid = false;
+                console.log('Component: Field "ports" contains a port that exists outside of the component at ' +
+                        this.ports[i].pos + ' at index ' + i + '.');
+
+            } else if (!((this.ports[i].pos.x === 0 || this.ports[i].pos.x === this.xSpan) ||
+                    (this.ports[i].pos.y === 0 || this.ports[i].pos.y === this.ySpan))) {
+
+                valid = false;
+                console.log('Component: Field "ports" contains a port in an invalid location ' + this.ports[i].pos + ' at' +
+                        'index ' + i + '.');
+            }
+        }
+
+        // Check invalid port layers
+        for (let i = 0; i < this.ports.length; i++) {
+            // Make sure we don't search layers if it still has the default value
+            if (this.layers && !this.layers.includes(this.ports[i].layer)) {
+                valid = false;
+                console.log('Component: Field "ports" contains a port with an invalid layer (' + this.ports[i].layer +
+                        ') at index ' + i + '.');
+            }
+        }
+
+        return valid;
+    }
+
+    /**
+     * Tests the given value against the rules of spans.
+     *
+     * Cannot be less than 1. Cannot equal the default span value.
+     *
+     * @since 1.0.0
+     *
+     * @param {number}  value A value to test against the span rules.
+     *
+     * @returns {string} A TestResult value depending on whether the the
+     *                   was default, invalid or valid.
+     */
+    static testSpan(value) {
+        if (value === Component.DEFAULT_SPAN_VALUE) {
+            return TestResult.DEFAULT;
+        }
+
+        if (value < 1) {
+            return TestResult.INVALID;
+        }
+
+        return TestResult.VALID;
+    }
+
+    /**
+     * The default x-/y-span values.
+     *
+     * @since 1.0.0
+     *
+     * @returns {number}
+     */
+    static get DEFAULT_SPAN_VALUE() {
+        return -1;
+    }
+}
+
+module.exports = Component;
---
+++ tests/component.test.js
@@ -0,0 +1,163 @@
+const ParchKey = require('../parch-key.js');
+const Component = require('../component.js');
+const Port = require('../port.js');
+const Coord = require('../coord.js');
+
+//Suppress console logs
+console.log = jest.fn();
+
+var l1 = 'layer-1-id';
+var l2 = 'layer-2-id';
+var p1 = new Port('port-1-label', l1, new Coord(0, 0));
+var p2 = new Port('port-2-label', l2, new Coord(0, 5));
+
+test('initialize Component: parameters', () => {
+    let cVal = new Component('name', 'id', [l1, l2], 10, 10, 'entity', [new Port(), new Port()]);
+
+    expect(cVal.name).toBe('name');
+    expect(cVal.id).toBe('id');
+    expect(cVal.layers).toEqual([l1, l2]);
+    expect(cVal.xSpan).toBe(10);
+    expect(cVal.ySpan).toBe(10);
+    expect(cVal.entity).toBe('entity');
+    expect(cVal.ports).toEqual([new Port(), new Port()]);
+});
+
+test('initialize Component: default', () => {
+    let cDef = new Component();
+
+    expect(cDef.name).toBe(ParchKey.DEFAULT_STR_VALUE);
+    expect(cDef.id).toBe(ParchKey.DEFAULT_STR_VALUE);
+    expect(cDef.layers).toBe(null);
+    expect(cDef.xSpan).toBe(Component.DEFAULT_SPAN_VALUE);
+    expect(cDef.ySpan).toBe(Component.DEFAULT_SPAN_VALUE);
+    expect(cDef.entity).toBe(ParchKey.DEFAULT_STR_VALUE);
+    expect(cDef.ports).toBe(null);
+});
+
+test('modify Component', () => {
+    let c = new Component();
+
+    c.name = 'name';
+    c.id = 'id';
+    c.layers = [l1, l2];
+    c.xSpan = 10;
+    c.ySpan = 15;
+    c.entity = 'entity';
+    c.ports = [new Port(), new Port()];
+
+    expect(c.name).toBe('name');
+    expect(c.id).toBe('id');
+    expect(c.layers).toEqual([l1, l2]);
+    expect(c.xSpan).toBe(10);
+    expect(c.ySpan).toBe(15);
+    expect(c.entity).toBe('entity');
+    expect(c.ports).toEqual([new Port(), new Port()]);
+});
+
+test('validate Component: valid values', () => {
+    let goodComp = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity', [p1, p2]);
+    expect(goodComp.validate()).toBe(true);
+});
+
+test('validate Component: invalid values', () => {
+    let badComp = new Component('', '', [l1, ''], -123, 0, '', [new Port(), new Port()]);
+    let badNameComp = new Component('', 'comp-id', [l1, l2], 10, 10, 'comp-entity', [p1, p2]);
+    let badIDComp = new Component('comp-name', '', [l1, l2], 10, 10, 'comp-entity', [p1, p2]);
+    let badLayerComp = new Component('comp-name', 'comp-id', [l1, ''], 10, 10, 'comp-entity', [p1, p2]);
+    let badXSpanComp = new Component('comp-name', 'comp-id', [l1, l2], -312423, 10, 'comp-entity', [p1, p2]);
+    let badYSpanComp = new Component('comp-name', 'comp-id', [l1, l2], 10, 0, 'comp-entity', [p1, p2]);
+    let badEntityComp = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, '', [p1, p2]);
+    let badPortComp = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity', [new Port(), new Port()]);
+
+    expect(badComp.validate()).toBe(false);
+    expect(badNameComp.validate()).toBe(false);
+    expect(badIDComp.validate()).toBe(false);
+    expect(badLayerComp.validate()).toBe(false);
+    expect(badXSpanComp.validate()).toBe(false);
+    expect(badYSpanComp.validate()).toBe(false);
+    expect(badEntityComp.validate()).toBe(false);
+    expect(badPortComp.validate()).toBe(false);
+});
+
+test('validate Component: duplicate port labels', () => {
+    let c = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-name', l1, new Coord(5, 0)), new Port('port-name', l1, new Coord(0, 5))]);
+
+    expect(c.validate()).toBe(false);
+});
+
+test('validate Component: invalid port layers', () => {
+    let c = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-1', 'bad-layer', new Coord(5, 0)), new Port('port-2', l1, new Coord(0, 5))]);
+
+    expect(c.validate()).toBe(false);
+});
+
+test('validate Component: overlapping port locations', () => {
+    let c = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord(5, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+
+    expect(c.validate()).toBe(false);
+});
+
+test('validate Component: port locations', () => {
+    let c = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord())]);
+
+    for (let x = 0; x <= c.xSpan + 1; x++) {
+        for (let y = 0; y <= c.ySpan + 1; y++) {
+            c.ports[0].pos.setLocation(x, y);
+
+            if (x > c.xSpan || y > c.ySpan) {
+                // outside of the component is invalid
+                expect(c.validate()).toBe(false);
+            } else if ((x === 0 || x === c.xSpan) || (y === 0 || y === c.ySpan)) {
+                // edges and corners are valid
+                expect(c.validate()).toBe(true);
+            } else {
+                // anywhere else is invalid
+                expect(c.validate()).toBe(false);
+            }
+        }
+    }
+
+    // Spot checks just in case the above logic is off
+    c.ports[0].pos.setLocation(0, 0);
+    expect(c.validate()).toBe(true);
+
+    c.ports[0].pos.setLocation(4, 10);
+    expect(c.validate()).toBe(true);
+
+    c.ports[0].pos.setLocation(3, 6);
+    expect(c.validate()).toBe(false);
+
+    c.ports[0].pos.setLocation(0, 456);
+    expect(c.validate()).toBe(false);
+});
+
+test('validate Component: default values', () => {
+    let def = new Component();
+    let defName = new Component(ParchKey.DEFAULT_STR_VALUE, 'comp-id', [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defID = new Component('comp-name', ParchKey.DEFAULT_STR_VALUE, [l1, l2], 10, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defLabel = new Component('comp-name', 'comp-id', null, 10, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defXSpan = new Component('comp-name', 'comp-id', [l1, l2], Component.DEFAULT_SPAN_VALUE, 10, 'comp-entity',
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defYSpan = new Component('comp-name', 'comp-id', [l1, l2], 10, Component.DEFAULT_SPAN_VALUE, 'comp-entity',
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defEntity = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, ParchKey.DEFAULT_STR_VALUE,
+            [new Port('port-1', l1, new Coord(0, 0)), new Port('port-2', l1, new Coord(5, 0))]);
+    let defPorts = new Component('comp-name', 'comp-id', [l1, l2], 10, 10, 'comp-entity', null);
+
+    expect(def.validate()).toBe(false);
+    expect(defName.validate()).toBe(false);
+    expect(defID.validate()).toBe(false);
+    expect(defLabel.validate()).toBe(false);
+    expect(defXSpan.validate()).toBe(false);
+    expect(defYSpan.validate()).toBe(false);
+    expect(defEntity.validate()).toBe(false);
+    expect(defPorts.validate()).toBe(false);
+});
