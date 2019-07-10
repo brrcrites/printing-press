1b2b6917c4a72c73f8750f8429f80d9806a7cafa
Michael Bradley <mbrad94@gmail.com>
2019-07-10 14:23:46 -0700

    Add ParchKey map validation to Validation class
    Helper function that is called on each map field in Architecture
    class.
    
    Add missing ':' to console logs.

--- utils/validation.js
+++ utils/validation.js
@@ -79,11 +79,11 @@ class Validation {
      */
     static testStringValue(value, field, caller) {
         if (!value) {
-            console.log(caller + 'Field "' + field + '" equates to falsey.');
+            console.log(caller + ': Field "' + field + '" equates to falsey.');
             return false;
         }
         if (typeof value !== 'string') {
-            console.log(caller + 'Field "' + field + '" is not a string.');
+            console.log(caller + ': Field "' + field + '" is not a string.');
             return false;
         }
         if (value === Validation.DEFAULT_STR_VALUE) {
@@ -110,7 +110,7 @@ class Validation {
      */
     static testSpanValue(value, axis, caller) {
         if (typeof value !== 'number') {
-            console.log(caller + 'Field "' + axis + '" is not a number.');
+            console.log(caller + ': Field "' + axis + '" is not a number.');
             return false;
         }
         if (value < 1) {
@@ -139,7 +139,7 @@ class Validation {
      */
     static testCoordValue(value, field, caller) {
         if (typeof value !== 'number') {
-            console.log(caller + 'Field "' + field + '" is not a number.');
+            console.log(caller + ': Field "' + field + '" is not a number.');
             return false;
         }
         if (value < 0) {
@@ -168,7 +168,7 @@ class Validation {
      */
     static testDimensionValue(value, field, caller) {
         if (typeof value !== 'number') {
-            console.log(caller + 'Field "' + field + '" is not a number.');
+            console.log(caller + ': Field "' + field + '" is not a number.');
             return false;
         }
         if (value < 0) {
@@ -177,6 +177,70 @@ class Validation {
         }
 
         return true;
+    }
+
+    /**
+     * THIS IS SLOW. REWRITE THIS USING A SET INSTEAD
+     * Check whether IDs are unique in the given array.
+     *
+     * @since 1.0.0
+     *
+     * @param {Array}   array   An array of objects that contain an id
+     *                          field.
+     * @param {string}  field   A string representation of the field name for
+     *                          the error message.
+     * @param {string}  caller  A string representation of the caller's class
+     *                          for the error message.
+     *
+     * @return {boolean}    true if the array contains unique IDs, false
+     *                      otherwise.
+     */
+    static testIDUniqueness(array, field, caller) {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i].id === array[j].id) {
+                    console.log(caller + ': Field "' + field + '" has matching IDs: "' + array[i] + '" at indices' +
+                            ' ' + i + ' and ' + j + '.');
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Convenience function that validates a map of ParchKey subclasses.
+     *
+     * The map size must be greater than zero. Each element must be valid.
+     *
+     * @since 1.0.0
+     *
+     * @param {Array}   map   An map of objects that subclass ParchKey.
+     * @param {string}  field   A string representation of the field name for
+     *                          the error message.
+     * @param {string}  caller  A string representation of the caller's class
+     *                          for the error message.
+     *
+     * @returns {boolean}   true if all objects in the map are valid, false
+     *                      otherwise.
+     */
+    static validateMap(map, field, caller) {
+        let valid = true;
+
+        if (map.size === 0) {
+            console.log(caller + ': Field "' + field + '" cannot be an empty map.');
+            return false;
+        }
+
+        for (let value of map.values()) {
+            if (!value.validate()) {
+                valid = false;
+                console.log(caller + ': Field "' + field + '" has an invalid element with ID "' + value.id + '".');
+            }
+        }
+
+        return valid;
     }
 }
 
