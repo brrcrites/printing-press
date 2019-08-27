ac3f6b9291f306be242427a7bd036695a1f4e776
Michael Bradley <mbrad94@gmail.com>
2019-08-27 16:05:32 -0700

    Change debug output
    I don't know bash very well, so I've added curly brackets around the
    ECRYPTED_* variables in the debug output to see if that changes
    anything. Although I have a feeling in this context it will make no
    difference.

--- deploy.sh
+++ deploy.sh
@@ -57,9 +57,9 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 echo "ENCRYPTED_IV_VAR: $ENCRYPTED_IV_VAR"
 echo "ENCRYPTED_IV: $ENCRYPTED_IV"
-echo "ENCRYPTED_KEY_VAR: $ENCRYPTED_KEY_VAR"
-echo "ENCRYPTED_KEY: $ENCRYPTED_KEY"
-openssl aes-256-cbc -K $ENCRYPTED_KEY_VAR -iv $ENCRYPTED_IV_VAR -in ../gh-pages-deploy-key.enc -out ../gh-pages-deploy-key -d
+echo "ENCRYPTED_KEY_VAR: ${ENCRYPTED_KEY_VAR}"
+echo "ENCRYPTED_KEY: ${ENCRYPTED_KEY}"
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../gh-pages-deploy-key.enc -out ../gh-pages-deploy-key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add ../deploy_key
