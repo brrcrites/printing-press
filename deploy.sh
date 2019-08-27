41bbf353307ae9f965f7b718dc633c3a510bf858
Michael Bradley <mbrad94@gmail.com>
2019-08-27 15:42:05 -0700

    Add debug output
    Print encrypted key/var variables.

--- deploy.sh
+++ deploy.sh
@@ -57,6 +57,8 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 echo "ENCRYPTED_IV_VAR: $ENCRYPTED_IV_VAR"
 echo "ENCRYPTED_IV: $ENCRYPTED_IV"
+echo "ENCRYPTED_KEY_VAR: $ENCRYPTED_KEY_VAR"
+echo "ENCRYPTED_KEY: $ENCRYPTED_KEY"
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../gh-pages-deploy-key.enc -out ../gh-pages-deploy-key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
